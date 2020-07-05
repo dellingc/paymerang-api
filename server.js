e2be983a96be016cc053c39b73fa0db5d4387718
dellingc <35514357+dellingc@users.noreply.github.com>
2020-07-05 18:32:39 -0400

    added a README

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const rawData = require('./Data/data.json');
-const { allData, indPayee } = require('./Controllers');
+const { allData, indPayee } = require('./Controllers/index.js');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Vary: origin");
     next();
